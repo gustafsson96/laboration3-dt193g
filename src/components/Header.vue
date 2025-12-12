21172862c6879dddf3c3a558e608649c4835fa5e
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-12-12 14:11:32 +0100

    Create basic navbar in Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,15 @@
 <template>
-
+    <nav>
+        <ul>
+            <li><RouterLink to="/">Hem</RouterLink></li>
+            <li><RouterLink to="/film">Film</RouterLink></li>
+            <li><RouterLink to="/info">Info</RouterLink></li>
+        </ul>
+    </nav>
 </template>
 
 <script setup>
-
+    import { RouterLink } from 'vue-router'
 </script>
 
 <style scoped>
