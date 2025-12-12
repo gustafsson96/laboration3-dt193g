c2c986363416a885f81db5b354e85348a646c427
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-12-12 15:54:23 +0100

    Add header element to header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,23 @@
 <template>
-    <nav>
-        <ul>
-            <li><RouterLink to="/">Hem</RouterLink></li>
-            <li><RouterLink to="/film">Film</RouterLink></li>
-            <li><RouterLink to="/info">Info</RouterLink></li>
-        </ul>
-    </nav>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <RouterLink to="/">Hem</RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/film">Film</RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/info">Info</RouterLink>
+                </li>
+            </ul>
+        </nav>
+    </header>
 </template>
 
 <script setup>
-    import { RouterLink } from 'vue-router'
+import { RouterLink } from 'vue-router'
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
