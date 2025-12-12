44562f16168cd651dcfabd98f7d9476473c46f0d
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-12-12 17:29:37 +0100

    Fetch movies to FilmView table from REST API

---
+++ src/components/FilmRow.vue
@@ -0,0 +1,17 @@
+<template>
+    <tr>
+        <td> {{ movie.title }}</td>
+        <td> {{ movie.year }}</td>
+        <td> {{movie.genre}}</td>
+        <td> {{movie.length}}</td>
+        <td> {{movie.watched}}</td>
+    </tr>
+</template>
+
+<script setup>
+const props = defineProps({
+    movie: Object
+})
+</script>
+
+<style scoped></style>
--- src/views/FilmView.vue
+++ src/views/FilmView.vue
@@ -1,5 +1,19 @@
 <template>
     <h1>Filmsidan!</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Titel</th>
+                <th>År</th>
+                <th>Genre</th>
+                <th>Längd</th>
+                <th>Tittad på</th>
+            </tr>
+        </thead>
+        <tbody>
+            <FilmRow v-for="movie in movies" :key="movie.id" :movie="movie" />
+        </tbody>
+    </table>
 
     <!--
      Undersida som konsumerar extern webbtjänst skapad i tidigare labb, förslagsvis till en tabell. 
@@ -7,9 +21,30 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
+import FilmRow from '@/components/FilmRow.vue';
+
+const movies = ref([])
+
+onMounted(() => {
+    getMovies();
+})
+
+const getMovies = async () => {
+    try {
+        const res = await fetch("https://laboration2-2-dt193g.onrender.com/movies");
+
+        if (res.ok) {
+            const data = await res.json();
+
+            movies.value = data;
+        }
+    }
+    catch (error) {
+
+    }
+}
 
 </script>
 
-<style>
-    
-</style>
+<style></style>
