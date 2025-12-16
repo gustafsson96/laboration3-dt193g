3af83ee968bea887bdb563b9a0aa9aa6be3d200f
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-12-16 13:37:10 +0100

    Update navbar with styling and responsive functionality

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,20 @@
 <template>
     <header>
         <nav>
-            <ul>
+            <button class="hamburger-icon" @click="toggleMenu" :class="{ open: menuOpen }" aria-label="toggle menu">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </button>
+            <ul :class="{ open: menuOpen }">
                 <li>
-                    <RouterLink to="/">Hem</RouterLink>
+                    <RouterLink to="/" @click="closeMenu" active-class="active-link">Hem</RouterLink>
                 </li>
                 <li>
-                    <RouterLink to="/film">Film</RouterLink>
+                    <RouterLink to="/film" @click="closeMenu" active-class="active-link">Film</RouterLink>
                 </li>
                 <li>
-                    <RouterLink to="/info">Info</RouterLink>
+                    <RouterLink to="/info" @click="closeMenu" active-class="active-link">Info</RouterLink>
                 </li>
             </ul>
         </nav>
@@ -18,6 +23,126 @@
 
 <script setup>
 import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
+
+const menuOpen = ref(false)
+
+// Toggle menu (true/false)
+function toggleMenu() {
+    menuOpen.value = !menuOpen.value
+}
+
+// Close menu when linked is clicked
+function closeMenu() {
+    menuOpen.value = false
+}
+
 </script>
 
-<style scoped></style>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap');
+
+nav {
+    position: relative;
+    z-index: 10;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 500;
+    background-color: #223a6d;
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    padding: 1em;
+    margin: 0;
+}
+
+
+ul {
+    list-style-type: none;
+    display: flex;
+    gap: 2em;
+    padding: 0 0 0 1em;
+    margin: 0;
+}
+
+a {
+    text-decoration: none;
+    color: #fff;
+    padding: 0.3em 0.5em;
+    border-radius: 5px;
+    transition: background-color 0.3s ease;
+}
+
+a:hover {
+    background-color: rgba(255,255,255,0.15);
+    border-radius: 5px;
+}
+
+.active-link {
+    border-bottom: 2px solid #fff;
+}
+
+.hamburger-icon {
+    display: none;
+    flex-direction: column;
+    justify-content: space-between;
+    border: none;
+    background: none;
+    height: 15px;
+    width: 30px;
+    cursor: pointer;
+    margin: 0 0 0 1em;
+}
+
+.bar {
+    display: block;
+    height: 2px;
+    background-color: #fff;
+    border-radius: 2px;
+}
+
+
+@media screen and (max-width: 768px) {
+
+    .hamburger-icon {
+        display: flex;
+        margin: 0.2em;
+    }
+
+    .bar {
+        display: flex;
+        transition: transform 0.3s ease, opacity 0.2s ease;
+    }
+
+    ul {
+        display: none;
+    }
+
+    /* Styling for when navbar is open */
+    ul.open {
+        display: flex;
+        flex-direction: column;
+        position: absolute;
+        top: 100%;
+        left: 0;
+        background-color: #223a6d;
+        width: 200px;
+        height: 100vh;
+        margin: 0;
+        padding: 1.5em;
+    }
+
+    /* Rotate hamburger bars into cross when navbar is opened */
+    .hamburger-icon.open span:nth-child(1) {
+        transform: rotate(45deg) translate(4px, 4px);
+    }
+
+    .hamburger-icon.open span:nth-child(2) {
+        opacity: 0;
+
+    }
+
+    .hamburger-icon.open span:nth-child(3) {
+        transform: rotate(-45deg) translate(4px, -4px);
+    }
+}
+</style>
