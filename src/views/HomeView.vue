cd3fd55f88542c7f6ce4dfc05e33624e13eab4d5
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-12-16 14:35:12 +0100

    Add hero image to start page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
+    <picture>
+        <source srcset="../assets/images/hero-img-sm.webp" media="(max-width: 768px)">
+        <img src="../assets/images/hero-img.webp" alt="Blond kvinna i en biosalong">
+    </picture>
     <h1>Startsidan!</h1>
-
-    <!-- Startsida med huvumeny, banner (stor bild), kort information om valt fritidsintresse -->
 </template>
 
 <script setup>
@@ -9,5 +11,8 @@
 </script>
 
 <style>
-    
+img {
+    width: 100%;
+    height: auto;
+}
 </style>
